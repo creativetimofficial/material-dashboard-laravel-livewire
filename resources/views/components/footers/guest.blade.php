0ed5a82d1476e9b411143bb84c509deb0292173d
AlexVlad95 <alex.vlad@updivision.com>
2021-12-12 18:08:20 +0200

    update

--- resources/views/components/footers/guest.blade.php
+++ resources/views/components/footers/guest.blade.php
@@ -3,10 +3,7 @@
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-12 col-md-6 my-auto">
                 <div class="copyright text-center text-sm text-white text-lg-start">
-                    © <script>
-                        document.write(new Date().getFullYear())
-
-                    </script>,
+                    © 2021,
                     made with <i class="fa fa-heart" aria-hidden="true"></i> by
                     <a href="https://www.creative-tim.com" class="font-weight-bold text-white" target="_blank">Creative
                         Tim </a> & <a href="https://www.updivision.com" class="font-weight-bold text-white" target="_blank">UPDIVISION</a>
