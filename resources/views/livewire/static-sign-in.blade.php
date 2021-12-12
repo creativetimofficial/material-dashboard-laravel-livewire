96470e3e3a9e1ea848357bb1a3e14cae0b4ea87a
AlexVlad95 <alex.vlad@updivision.com>
2021-12-12 18:47:07 +0200

    update

--- resources/views/livewire/static-sign-in.blade.php
+++ resources/views/livewire/static-sign-in.blade.php
@@ -1,8 +1,4 @@
 
-    <main class="main-content  mt-0">
-        <div class="page-header align-items-start min-vh-100"
-            style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
-            <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container my-auto">
                 <div class="row">
                     <div class="col-lg-4 col-md-8 col-12 mx-auto">
@@ -59,6 +55,3 @@
                     </div>
                 </div>
             </div>
-            <x-footers.guest></x-footers.guest>
-        </div>
-    </main>
